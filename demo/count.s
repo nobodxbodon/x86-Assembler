e3a33da1e76628782afa673ae41b662f953158d3
FromWhereToWhere <fromwheretowhere.service@gmail.com>
2017-02-14 23:05:11 -0800

    demo code with Chinese keyword

--- demo/count.s
+++ demo/count.s
@@ -3,14 +3,14 @@
 # The calling convention is that all registers are caller-save.
 
 main:
-	movl   %eax, $0
+	赋   %eax, $0
 main_loop:
 	pushl  %eax
 	call   $print_int
 	call   $delay
 	popl   %eax
 	incl   %eax
-	cmpl   %eax, $100
+	cmpl   %eax, $8
 	jne    $main_loop
 	
 	call   $exit
@@ -18,17 +18,17 @@ main_loop:
 
 # Prints the integer contained in %eax, in base 10.
 print_int:
-	movl   %ebp, %esp  # Preserve ESP
-	movl   %ecx, $0    # Count
+	赋   %ebp, %esp  # Preserve ESP
+	赋   %ecx, $0    # Count
 	
 	# New line character at the end
 	decl   %esp
 	movb   (%esp), $10
 	incl   %ecx
 	
-	movl   %ebx, $10  # Divisor
+	赋   %ebx, $10  # Divisor
 print_int_loop:
-	movl   %edx, $0
+	赋   %edx, $0
 	divl   %ebx
 	addl   %edx, $48
 	decl   %esp
@@ -38,19 +38,19 @@ print_int_loop:
 	jne    $print_int_loop
 	
 	# System call write
-	movl   %eax, $4    # System call #4 (sys_write)
-	movl   %ebx, $1    # File descriptor 1 (stdout)
-	movl   %edx, %ecx  # Buffer length
-	movl   %ecx, %esp  # Buffer
+	赋   %eax, $4    # System call #4 (sys_write)
+	赋   %ebx, $1    # File descriptor 1 (stdout)
+	赋   %edx, %ecx  # Buffer length
+	赋   %ecx, %esp  # Buffer
 	int    $0x80
 	
-	movl   %esp, %ebp  # Restore ESP
+	赋   %esp, %ebp  # Restore ESP
 	ret
 
 
 # Returns after a delay.
 delay:
-	movl   %eax, $300000000
+	赋   %eax, $300000000
 delay_loop:
 	decl   %eax
 	jnz    $delay_loop
@@ -59,6 +59,6 @@ delay_loop:
 
 # The exit system call (does not return)
 exit:
-	movl   %eax, $1    # System call #1 (sys_exit)
-	movl   %ebx, $0    # Exit code 0
+	赋   %eax, $1    # System call #1 (sys_exit)
+	赋   %ebx, $0    # Exit code 0
 	int    $0x80
